97a8ef8d6ffd271a37bd0340ae11da1f8c6c2947
SlenderFox <SlenderFox@outlook.com>
2022-11-11 18:15:52 +1100

    Reorganised directory

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Game.h"
+
+int main()
+{
+	Game* app = new Game();
+	app->Run();
+	//delete app; // This causes an error
+
+	std::cout << std::endl << "\tPress 'Enter' to exit the program.";
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+	std::cin.get();
+
+	return 0;
+}
