be15bb533c3e40d9e9ad27a4271816fc10d797aa
SlenderFox <SlenderFox@outlook.com>
2022-12-03 15:12:40 +1100

    Even more formatting to GameDefines.hpp

--- src/GameDefines.hpp
+++ src/GameDefines.hpp
@@ -1,26 +1,26 @@
 #pragma once
 
-typedef signed char			int8;
-typedef signed short			int16;
-typedef signed int			int32;
-typedef signed long long	int64;
-typedef unsigned char		uint8;
-typedef unsigned short		uint16;
-typedef unsigned int			uint32;
-typedef unsigned long long	uint64;
+typedef signed char        int8;
+typedef signed short       int16;
+typedef signed int         int32;
+typedef signed long long   int64;
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
 
-constexpr int8		int8_min		= -127-1;
-constexpr int16	int16_min	= -32767-1;
-constexpr int32	int32_min	= -2147483647-1;
-constexpr int64	int64_min	= -9223372036854775807-1;
-constexpr int8		INT8_MAX		= 127;
-constexpr int16	INT16_MAX	= 32767;
-constexpr int32	INT32_MAX	= 2147483647;
-constexpr int64	INT64_MAX	= 9223372036854775807;
-constexpr uint8	UINT8_MAX	= 0xff;
-constexpr uint16	UINT16_MAX	= 0xffff;
-constexpr uint32	UINT32_MAX	= 0xffffffff;
-constexpr uint64	UINT64_MAX	= 0xffffffffffffffff;
+constexpr int8    int8_min    = -127-1;
+constexpr int16   int16_min   = -32767-1;
+constexpr int32   int32_min   = -2147483647-1;
+constexpr int64   int64_min   = -9223372036854775807-1;
+constexpr int8    INT8_MAX    = 127;
+constexpr int16   INT16_MAX   = 32767;
+constexpr int32   INT32_MAX   = 2147483647;
+constexpr int64   INT64_MAX   = 9223372036854775807;
+constexpr uint8   UINT8_MAX   = 0xff;
+constexpr uint16  UINT16_MAX  = 0xffff;
+constexpr uint32  UINT32_MAX  = 0xffffffff;
+constexpr uint64  UINT64_MAX  = 0xffffffffffffffff;
 
 constexpr uint8 CHARWIDTH = 8;           // The width of one character in the console
 constexpr uint8 WIDTH = 90;              // The character width of the console window (How many characters wide)
@@ -31,13 +31,13 @@ constexpr int32 STARTINGCASH = 10000;    // The amount of cash the players start
 
 // Character storage
 // Double lines
-constexpr const char *const VERTICAL = "\xBA";
-constexpr const char *const TOP_RIGHT = "\xBB";
-constexpr const char *const BOTTOM_RIGHT = "\xBC";
-constexpr const char *const BOTTOM_LEFT = "\xC8";
-constexpr const char *const TOP_LEFT = "\xC9";
-constexpr const char *const HORIZONTAL = "\xCD";
+constexpr const char *const VERTICAL      = "\xBA";
+constexpr const char *const TOP_RIGHT     = "\xBB";
+constexpr const char *const BOTTOM_RIGHT  = "\xBC";
+constexpr const char *const BOTTOM_LEFT   = "\xC8";
+constexpr const char *const TOP_LEFT      = "\xC9";
+constexpr const char *const HORIZONTAL    = "\xCD";
 // Characters
 constexpr const char *const FORWARD_SLASH = "\x2F";
-constexpr const char *const BACK_SLASH = "\x5C";
-constexpr const char *const FLAT_LINE = "\xC4";
+constexpr const char *const BACK_SLASH    = "\x5C";
+constexpr const char *const FLAT_LINE     = "\xC4";
