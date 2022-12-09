49ac4e2004b0102a067cc02edd0063223e3acac6
SlenderFox <SlenderFox@outlook.com>
2022-12-09 17:33:15 +1100

    Commented buffer.h

--- src/buffer.h
+++ src/buffer.h
@@ -7,21 +7,42 @@ typedef struct
 	uint32 m_rows, m_columns;
 } st_buffer;
 
+/** Allocates memory for the buffer struct
+ * @param _buf The buffer
+ */
 void
 st_buffer_construct (st_buffer **_buf);
 
+/** Frees the memory for the buffer struct
+ * @param _buf The buffer
+ */
 void
 st_buffer_destruct (st_buffer **_buf);
 
+/** Allocates memory for the data stored inside the buffer struct
+ * @param _buf The buffer
+ */
 void
 st_buffer_data_init (st_buffer *_buf);
 
+/** Frees the memory for the data stored inside the buffer struct
+ * @param _buf The buffer
+ */
 void
 st_buffer_data_terminate (st_buffer *_buf);
 
+/** Sets all the memory inside the data of a buffer to a space character
+ * @param _buf The buffer
+ */
 void
 st_buffer_data_clear (st_buffer *_buf);
 
+/** Sets a char at a specific location in a buffers data
+ * @param _buf The buffer
+ * @param _row The row
+ * @param _column The column
+ * @param _val The character
+ */
 void
 st_buffer_data_set (
 	st_buffer *_buf,
@@ -30,6 +51,12 @@ st_buffer_data_set (
 	char _val
 );
 
+/** Gets a char at a specific location in a buffers data
+ * @param _buf The buffer
+ * @param _row The row
+ * @param _column The column
+ * @return [char] The character
+ */
 char
 st_buffer_data_at (
 	st_buffer *_buf,
@@ -37,8 +64,16 @@ st_buffer_data_at (
 	uint32 _column
 );
 
+/** Returns the amount of rows in a buffer
+ * @param _buf The buffer
+ * @return [uint32] The amount of rows
+ */
 uint32
 st_buffer_get_rows (st_buffer *_buf);
 
+/** Returns the amount of columns in a buffer
+ * @param _buf The buffer
+ * @return [uint32] The amount of columns
+ */
 uint32
 st_buffer_get_columns (st_buffer *_buf);
