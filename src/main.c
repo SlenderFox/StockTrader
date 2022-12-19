956e12e998e0cc1e490b62bb3af5bf2e92fd4d84
SlenderFox <SlenderFox@outlook.com>
2022-12-19 22:09:52 +1100

    Added a loading progress for some reason

--- src/main.c
+++ src/main.c
@@ -1,27 +1,58 @@
-//#include "io.h"
-#include <stdlib.h>
-#include <stdio.h>
+#define _POSIX_C_SOURCE 200112L // For nanosleep to be used without warning
+
+#include "io.h"
+#include <stdbool.h> // bool, true, false
+#include <stdio.h> // printf, fflush
+#include <time.h> // timespec, nanosleep
+#include <errno.h> // errno
+
+/* TODO:
+ * Input checking
+ * Loading info into buffer
+ * Redraw for linux
+ * Compare replace for Windows
+*/
 
 int
-main (int argc, char **args)
+main (int argc, char *args[])
 {
-	char *envvar = getenv ("SHELL");
-	if (envvar == NULL) envvar = "NULL\0";
-	printf ("SHELL: %s\n", envvar);
+	//char *envvar = getenv ("SHELL");
+	//if (envvar == NULL) envvar = "NULL\0";
+	//printf ("SHELL: %s\n", envvar);
 
-	envvar = getenv ("HOME");
-	if (envvar == NULL) envvar = "NULL\0";
-	printf ("HOME: %s\n", envvar);
+	int percent_complete = 0;
+	struct timespec ts_requested =
+	{
+		.tv_sec = 0, // 0 seconds
+		.tv_nsec = 10 * 1000000 // 10 milliseconds in nanoseconds
+	};
+	struct timespec ts_remaining;
+	while (percent_complete < 100)
+	{
+		printf ("\rProgress: %d%%", percent_complete);
+		fflush (stdout);
+		++percent_complete;
+		int ret = nanosleep (&ts_requested, &ts_remaining);
+		if (ret == -1)
+		{
+			return errno;
+		}
+	}
+	// Finally print 100% and a newline
+	printf ("\rProgress: %d%%\n", percent_complete);
 
-	envvar = getenv ("ROOT");
-	if (envvar == NULL) envvar = "NULL\0";
-	printf ("ROOT: %s\n", envvar);
+	bool play = true;
+	st_io_init ();
 
-	//st_io_init ();
-	//st_io_draw ();
-	//st_io_update ();
-	//st_io_draw ();
-	//st_io_terminate ();
+	while (play)
+	{
+		// Outputs the contents buffer to the console
+		st_io_draw ();
+
+		play = false;
+	}
+
+	st_io_terminate ();
 
 	return 0;
 }
